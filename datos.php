bb8dc7daed778f326c0e5f6481f3b63e9903f985
Rzjesusgil <ilfrandejesusgil@gmail.com>
2024-08-08 15:23:29 -0500

    Update datos.php

--- datos.php
+++ datos.php
@@ -5,6 +5,8 @@ $producto3=$_POST [ 'producto3'];
 $precio1=$_POST ['precio1'];
 $precio2=$_POST ['precio2'];
 $precio3=$_POST ['precio3'];
+suma=$precio1+$precio2+$precio3;
+$descuento=$suma-0,16%;
 echo "el nombre del producto 1 es: ". $producto1;
 echo'<br>'
   echo "el nombre del producto 2 es: ". $producto2;

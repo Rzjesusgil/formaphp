ac89efd430d96fe3a2695456f7b661d71b87d30e
Rzjesusgil <ilfrandejesusgil@gmail.com>
2024-08-13 15:29:03 -0500

    Update datos.php

--- datos.php
+++ datos.php
@@ -5,19 +5,22 @@ $producto3=$_POST [ 'producto3'];
 $precio1=$_POST ['precio1'];
 $precio2=$_POST ['precio2'];
 $precio3=$_POST ['precio3'];
-suma=$precio1+$precio2+$precio3;
-$descuento=$suma-0,16%;
-echo "el nombre del producto 1 es: ". $producto1;
-echo'<br>'
-  echo "el nombre del producto 2 es: ". $producto2;
-echo'<br>'
-  echo "el nombre del producto 3 es: ". $producto3;
-echo'<br>'
-  echo "el precio del producto 1 es: ". $precio3;
-echo'<br>'
-  echo "el precio del product 2 es: ". $precio3;
-echo'<br>'
-  echo "el precio del producto 3 es: ". $precio3
-echo'<br>'
+$suma=$precio1+$precio2+$precio3;
+
+$descuento=$suma*0.16;
+$descuentoo=$suma-$descuento;
+
+echo " producto1: ".$producto1.'='.$precio1;
+echo '<br>';
+   echo "producto2: ".$producto2.'= '.$precio2;
+   echo '<br>';
+
+echo "producto3: ".$producto3.'='.$precio3;
+
+ echo '<br>';
+ echo "LA SUMA DE LOS PEODUCTOS ES : ".$suma;
+echo'<br>';
+        
+        echo "EL TOTAL A PAGA ES: ".$descuentoo ;
 
 ?>
